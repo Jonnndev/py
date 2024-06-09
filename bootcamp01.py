9191452ab49e75d7b490133141af1873998a22f2
santossdev <kyan.zipp@gmail.com>
2024-06-09 02:25:42 -0300

    exercicios do bootcamp
    day two

---
+++ bootcamp01.py
@@ -0,0 +1,46 @@
+#Adiçao
+print(10+2)
+
+#Subtraçao
+print(10-2)
+
+#Divisao
+print(10/3)
+
+#Divisao Inteira
+print(10//3)
+
+#Multiplicaçao
+print(10*2)
+
+
+#Modulo é o resto da divisao
+print(10%3)
+
+#Exponenciaçao
+print(2**3)
+
+
+
+produto_1 = 10
+produto_2 = 20
+
+
+print(produto_2 + produto_1)
+print(produto_2 - produto_1)
+print(produto_2 / produto_1)
+print(produto_2 // produto_1)
+print(produto_2 * produto_1)
+print(produto_2 ** produto_1)
+print(produto_2 % produto_1)
+print(10+2*10-2)
+print(8**2 + (10 - 2) / 2)
+
+
+
+
+
+valor_1 = float(input("Digite o primeiro numero "))
+valor_2 = float(input("Digite o segundo numero "))
+soma = valor_1+valor_2
+print(soma)
---
+++ bootcamp02.py
@@ -0,0 +1,19 @@
+saldo = 300
+saque = 200
+
+#Igualdade
+print(saldo == saque)
+
+#Diferença
+print(saldo != saque)
+#Maior que
+print(saldo > saque)
+
+#Maior ou igual
+print(saldo >= saque)
+
+#Menor que
+print(saldo < saque)
+
+#Menor ou igual
+print(saldo <= saque)
